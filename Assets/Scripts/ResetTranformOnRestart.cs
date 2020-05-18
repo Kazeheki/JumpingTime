661a273e0de9723fe572657675a9d05ec1a43b20
Kazeheki <kazeheki@web.de>
2020-05-18 11:37:32 +0200

    #6 add position/rotation reset on game restart to clock handles

---
+++ Assets/Scripts/ResetTranformOnRestart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ResetTranformOnRestart : MonoBehaviour
+{
+    private Vector3 m_InitialPosition;
+    private Quaternion m_InitialRotation;
+
+    private void Awake()
+    {
+        m_InitialPosition = transform.position;
+        m_InitialRotation = transform.rotation;
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.OnRestart += Reset;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnRestart -= Reset;
+    }
+
+    private void Reset()
+    {
+        transform.position = m_InitialPosition;
+        transform.rotation = m_InitialRotation;
+    }
+}
