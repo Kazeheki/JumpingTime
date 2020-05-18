05eaf2885b61d92face888fd7371f7dc81a30205
Kazeheki <kazeheki@web.de>
2020-05-18 19:43:28 +0200

    #5 add score UI

---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI m_ScoreTextField = null;
+
+    private void OnEnable()
+    {
+        GameEvents.OnUpdateScoreVisuals += SetScoreText;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnUpdateScoreVisuals -= SetScoreText;
+    }
+
+    private void SetScoreText(int score)
+    {
+        m_ScoreTextField.text = score.ToString();
+    }
+}
