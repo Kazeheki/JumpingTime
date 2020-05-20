d4a08d034415e6fc09c6bf1deb87ee0e51a2adfe
Kazeheki <kazeheki@web.de>
2020-05-20 21:22:03 +0200

    #21 add soundManager and coin collection sound

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Sound
+{
+    [HideInInspector]
+    public AudioSource Source;
+
+    public string Name;
+
+    public AudioClip Clip;
+
+    [Range(0f, 1f)]
+    public float Volume = 1f;
+
+    public bool Loop;
+}
