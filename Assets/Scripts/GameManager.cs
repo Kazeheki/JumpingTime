ccf96dbf69b7c4405c0f109049ca685b0b14bb25
Kazeheki <kazeheki@web.de>
2020-05-21 21:08:47 +0200

    #13 add pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private string m_PauseMenuSceneName = "PauseMenu";
+
     private static bool m_GameOver;
 
     private void OnEnable()
     {
         GameEvents.OnGameOver += OnGameOver;
         GameEvents.OnRestart += OnRestart;
+        GameEvents.OnResume += ResumeGame;
     }
 
     private void OnDisable()
     {
         GameEvents.OnGameOver -= OnGameOver;
         GameEvents.OnRestart -= OnRestart;
+        GameEvents.OnResume -= ResumeGame;
     }
 
     private void OnGameOver()
@@ -28,12 +35,32 @@ public class GameManager : MonoBehaviour
         Debug.Log("Restarting Game");
         Time.timeScale = 1;
         m_GameOver = false;
+
+        if (SceneManager.GetSceneByName(m_PauseMenuSceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(m_PauseMenuSceneName);
+        }
     }
 
     public void RestartGame()
     {
         Debug.Log("Triggered Restart");
         GameEvents.Restart();
+    }
+
+    public void PauseGame(InputAction.CallbackContext context)
+    {
+        if (context.performed && !SceneManager.GetSceneByName(m_PauseMenuSceneName).isLoaded)
+        {
+            Time.timeScale = 0;
+            SceneManager.LoadScene(m_PauseMenuSceneName, LoadSceneMode.Additive);
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.UnloadSceneAsync(m_PauseMenuSceneName);
     }
 
     public static bool IsGameRunning => !m_GameOver;
