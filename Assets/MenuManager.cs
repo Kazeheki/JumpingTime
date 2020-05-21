19c41efa1d41238e97c34acd8e46b0d9f4d411a6
Kazeheki <kazeheki@web.de>
2020-05-21 20:00:49 +0200

    #7 add main menu and change audioMixer to work with it

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixerSnapshot m_Standard = null;
+
+    [SerializeField]
+    private AudioMixerSnapshot m_Muted = null;
+
+    [SerializeField]
+    private AudioMixerSnapshot m_MusicMuted = null;
+
+    private float m_AudioTransitionTime = 1f;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ToggleMuteAll(bool value)
+    {
+        if (value)
+        {
+            m_Muted.TransitionTo(m_AudioTransitionTime);
+        }
+        else
+        {
+            m_Standard.TransitionTo(m_AudioTransitionTime);
+        }
+    }
+
+    public void ToggleMuteMusic(bool value)
+    {
+        if (value)
+        {
+            m_MusicMuted.TransitionTo(m_AudioTransitionTime);
+        }
+        else
+        {
+            m_Standard.TransitionTo(m_AudioTransitionTime);
+        }
+    }
+}
