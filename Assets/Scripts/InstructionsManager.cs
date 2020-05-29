117dce3d40a20a530ae4fd0f45a9cefe3a90cc93
Kazeheki <kazeheki@web.de>
2020-05-29 17:26:04 +0200

    fix pause menu not working

--- Assets/Scripts/InstructionsManager.cs
+++ Assets/Scripts/InstructionsManager.cs
@@ -1,17 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InstructionsManager : MonoBehaviour
 {
+    [SerializeField]
+    private InputAction m_StartAction;
+
     private void Awake()
     {
         gameObject.SetActive(true);
         GameEvents.PauseGame();
+
+        m_StartAction.performed += OnClickStart;
+        m_StartAction.Enable();
     }
 
-    public void OnClickStart()
+    private void OnClickStart(InputAction.CallbackContext ctx)
     {
+        if (!ctx.performed)
+        {
+            return;
+        }
+
         GameEvents.ResumeGame();
 
         gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        m_StartAction.Disable();
     }
 }
