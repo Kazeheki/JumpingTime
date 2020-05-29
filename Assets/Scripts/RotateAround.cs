d79ee148085ffc901f7ef091978c49c46ad50522
Kazeheki <kazeheki@web.de>
2020-05-29 13:11:34 +0200

    #48 add difficulty increasing with coins

--- Assets/Scripts/RotateAround.cs
+++ Assets/Scripts/RotateAround.cs
@@ -14,9 +14,18 @@ public class RotateAround : MonoBehaviour
     [SerializeField]
     private bool m_AroundZ = false;
 
+    [SerializeField]
+    private Difficulty m_Difficulty = null;
+
     private void Update()
     {
-        float rotationValue = m_RotationSpeed * Time.deltaTime;
+        float calculatedSpeed = m_RotationSpeed;
+        if (m_Difficulty != null)
+        {
+            Debug.Log("Speed Multiplyer: " + m_Difficulty.SpeedMultiplyer);
+            calculatedSpeed *= m_Difficulty.SpeedMultiplyer;
+        }
+        float rotationValue = calculatedSpeed * Time.deltaTime;
         this.transform.rotation *= Quaternion.Euler(m_AroundX ? rotationValue : 0, m_AroundY ? rotationValue : 0, m_AroundZ ? rotationValue : 0);
     }
 }
